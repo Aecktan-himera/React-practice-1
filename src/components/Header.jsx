5a9e058346005506158bc50f3d110dd5c2877246
Aecktan-himera <aecktan-himera@mail.ru>
2026-04-18 20:55:41 +0300

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import Logo from "../assets/logo.svg";
+import Cart from "../assets/cart.svg";
+import Login from "../assets/login.svg";
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <div className="header-container">
+          <div className="logo-group">
+            <div className="logo-container">
+              <a href="#">
+                <img src={Logo} alt="logo" />
+              </a>
+            </div>
+            <nav className="filter-buttons">
+              <button className="btn menu-btn active">TV</button>
+              <button className="btn menu-btn non-active">Phone</button>
+              <button className="btn menu-btn non-active">Laptop</button>
+            </nav>
+          </div>
+          <div className="buttons-container">
+            <button className="btn right-btn">
+              <img src={Cart} alt="cart" />
+            </button>
+            <button className="btn right-btn">
+              <img src={Login} alt="login" />
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
